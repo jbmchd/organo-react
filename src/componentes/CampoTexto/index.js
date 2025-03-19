8bd407f24c27299bf427faed04e7264cd8453778
Joabe Machado <joabemachadodeabreu@gmail.com>
2025-03-19 18:24:47 -0300

    controlando estados

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,10 +2,21 @@ import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
     const placeholderModificada = `${props.placeholder}...`;
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} type="text" placeholder={placeholderModificada} />
+            <input 
+                type="text"
+                placeholder={placeholderModificada} 
+                required={props.obrigatorio} 
+                value = {props.valor}
+                onChange = {aoDigitado}
+            />
         </div>  
     )
 }
