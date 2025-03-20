75d24a44d154ed07e476a6a3a98e1039bf4ad5c3
Joabe Machado <joabemachadodeabreu@gmail.com>
2025-03-20 18:22:15 -0300

    ajustes finais

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,7 +4,8 @@ const ListaSuspensa = (props) => {
     return (
         <div className="lista-suspensa">
             <label>{props.label}</label>
-            <select required={props.obrigatorio} value={props.value} onChange={evento => props.aoAlterado(evento.target.value)} >
+            <select required={props.obrigatorio} value={props.valor} onChange={evento => props.aoAlterado(evento.target.value)}>
+                <option value=""></option>
                 {
                     props.itens.map(item => <option key={item}>{item}</option>)
                 }
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -18,6 +18,7 @@ const Time = (props) => {
                         nome={colaborador.nome} 
                         cargo={colaborador.cargo} 
                         imagem={colaborador.imagem} 
+                        corDeFundo={props.corPrimaria}
                 />)}
             </div>
         </section>
